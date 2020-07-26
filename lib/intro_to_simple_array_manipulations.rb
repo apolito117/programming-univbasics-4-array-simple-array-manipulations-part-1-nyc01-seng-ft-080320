c15a16d1297b444955ea14d7f0b73e031d878f96
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-07-26 16:18:33 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+
+
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(continents)
+  continents.pop
+end
+
+def pop_with_args (dog_breeds)
+  dog_breeds.pop(2)
+end
+
+def using_shift (my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def shift_with_args (ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
